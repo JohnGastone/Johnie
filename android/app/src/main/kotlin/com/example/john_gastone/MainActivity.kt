960985c7f7305afa42311847929b56c974c5fac1
JohnGastone <johngastone11@gmail.com>
2023-05-08 10:24:25 +0300

    First commit

---
+++ android/app/src/main/kotlin/com/example/john_gastone/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.john_gastone
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/pages/profile.dart
@@ -0,0 +1,21 @@
+// ignore_for_file: camel_case_types
+
+import 'package:flutter/material.dart';
+
+class profile extends StatelessWidget {
+  const profile({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: const [
+          SizedBox(
+            height: 150,
+          ),
+          Text('Profile')
+        ],
+      ),
+    );
+  }
+}
