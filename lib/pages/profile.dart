a1b311efc376a30902e63c6df1035913f0d54af9
JohnGastone <johngastone11@gmail.com>
2023-05-08 10:39:58 +0300

    +++

--- lib/pages/profile.dart
+++ lib/pages/profile.dart
@@ -13,10 +13,10 @@ class profile extends StatelessWidget {
           SizedBox(
             height: 150,
           ),
-          Text(
-            'Profile',
-            style: TextStyle(fontSize: 15, fontWeight: FontWeight.bold),
-          )
+          // Text(
+          //   'Profile',
+          //   style: TextStyle(fontSize: 15, fontWeight: FontWeight.bold),
+          // )
         ],
       ),
     );
