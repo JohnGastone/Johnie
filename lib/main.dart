80b1e26ca25ec1fd057c2b9d2c431b623cd4e540
JohnGastone <johngastone11@gmail.com>
2023-07-27 11:09:04 +0300

    ++

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables, use_key_in_widget_constructors, library_private_types_in_public_api
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables, use_key_in_widget_constructors, library_private_types_in_public_api, unused_field
 
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
@@ -29,7 +29,7 @@ class _ExampleState extends State<Example> {
   static const TextStyle optionStyle =
       TextStyle(fontSize: 30, fontWeight: FontWeight.w600);
   static const List<Widget> _widgetOptions = <Widget>[
-    home(),
+    Home(),
     favorite(),
     experience(),
     profile()
@@ -58,7 +58,7 @@ class _ExampleState extends State<Example> {
             child: GNav(
               rippleColor: Colors.grey[300]!,
               hoverColor: Colors.grey[100]!,
-              gap: 8,
+              gap: 6,
               activeColor: Colors.black,
               iconSize: 24,
               padding: EdgeInsets.symmetric(horizontal: 20, vertical: 12),
@@ -76,7 +76,7 @@ class _ExampleState extends State<Example> {
                 ),
                 GButton(
                   icon: CupertinoIcons.device_laptop,
-                  text: 'X-perience',
+                  text: 'Experience',
                 ),
                 GButton(
                   icon: CupertinoIcons.person_alt,
